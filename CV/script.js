6abc9f052e76637ab51b782e1477ec07abf8f6e0
yeny.lopez <yenylopezmendoza@outlook.es>
2019-08-28 00:05:26 -0500

    media querys

--- CV/script.js
+++ CV/script.js
@@ -26,8 +26,8 @@ let about = document.getElementById("about");
 let getAbout = document.getElementById("getAbout");
 let resume = document.getElementById("resume");
 let getResume = document.getElementById("getResume");
-let works = document.getElementById("works");
-let getBlog = document.getElementById("getBlog");
+let blog = document.getElementById("interest")
+let getBlog = document.getElementById("getInterest");
 let contact = document.getElementById("contact");
 let getContact = document.getElementById("getContact");
 
@@ -36,8 +36,6 @@ function remove() {
     getAbout.classList.remove('selected');
     resume.classList.remove('view');
     getResume.classList.remove('selected');
-    works.classList.remove('view');
-    getWorks.classList.remove('selected');
     blog.classList.remove('view');
     getBlog.classList.remove('selected');
     contact.classList.remove('view');
@@ -59,14 +57,6 @@ getResume.addEventListener('click', function (e) {
         remove();
         resume.classList.add('view');
         getResume.classList.add('selected');
-    }
-})
-getWorks.addEventListener('click', function (e) {
-    if (window.innerWidth > 1040) {
-        e.preventDefault();
-        remove();
-        works.classList.add('view');
-        getWorks.classList.add('selected');
     }
 })
 getBlog.addEventListener('click', function (e) {
