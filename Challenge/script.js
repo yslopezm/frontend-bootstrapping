ed83ffaae81202682e7b048038ddf845af260026
yeny.lopez <yenylopezmendoza@outlook.es>
2019-08-27 23:56:03 -0500

    JS refactor and grid with flex

--- Challenge/script.js
+++ Challenge/script.js
@@ -1,84 +1,76 @@
-
- 
 /**
  * Definition of the variables 
  */
-let photos = document.querySelector('.container')
+let photos = document.querySelector('#photos')
 let browser = document.querySelector('#users')
 const offset = 10000;
-const API = 'https://randomuser.me/api/?results=13';
+const API = 'https://randomuser.me/api/?results=100';
 let results;
-
+let lengthUsers;
 
 /**
  * Query
  */
 function queryAPI () {
-    fetch(API).then((value) =>{   
-        console.log(value)
+    fetch(API).then((value) =>{
         return value.json();
     }).then((value) => {
-        console.log(value)
-        createPhoto(value)
+        let results = value.results;
+        lengthUsers = results.length;
+        createPhoto(value);
     })
 }
 
 /**
  * @param {Input with the users array with all data} user
  */
-function createPhoto(user) {
-    results = user.results;
-    
-    for (let index = 0; index < 13; index++) {
-        console.log(user.results[index])        
-        localStorage.setItem('datos', JSON.stringify(user.results[index]));
-        let saved = localStorage.getItem('datos');
-        let article1 = document.createElement('article')
+function createPhoto(user) {   
 
+    for (let index = 0; index < lengthUsers; index++) {
+
+        localStorage.setItem('datos', JSON.stringify(user.results[index]));
+        
+        // new DOM elements 
+        let saved = localStorage.getItem('datos');
+        let article = document.createElement('article')
         let nameImg = document.createElement('div')
         let imagen = document.createElement('img')
-
         let clickImage = document.createElement('a')
 
-        article1.appendChild(imagen)
-        article1.appendChild(nameImg)
+        // Data extraction
+        let location = user.results[index].location.street
+        let city = user.results[index].location.city
+        let phone = user.results[index].phone
 
-        nameImg.innerHTML = user.results[index].name.first
-
-        // Image properties
+        // Image properties 
+        nameImg.innerHTML = user.results[index].name.first       
         imagen.src = user.results[index].picture.large
         imagen.alt = user.results[index].name.first
         imagen.className = user.results[index].name.first
         clickImage.href = user.results[index].picture.large
         clickImage.id = user.results[index].name.first
 
-        let location = user.results[index].location.street
-        let city = user.results[index].location.city
-        let phone = user.results[index].phone
-
+        // DOM structure
         imagen.id = index;
-
+        article.appendChild(imagen)
+        article.appendChild(nameImg)        
+        photos.appendChild(article)
         browser.appendChild(clickImage)
-
-        photos.appendChild(article1)
-
     }
 
 }
 
 
-let theParent = document.querySelector('.container');
-theParent.addEventListener('click', doSomething, false);
-function doSomething(e) {
 
+photos.addEventListener('click', selectImage, false);
+function selectImage(e) {
     if (e.target !== e.currentTarget) {
-
         alert('Hello');
-        let data = e.target.getAttribute('id'),
-            url = data;
-           history.pushState(results[data],'detail','personaje.html');
-
-    } e.stopPropagation();
+        // let data = e.target.getAttribute('id'),
+        //     url = data;
+        //     history.pushState(results[data],'detail','character.html');
+        // console.log(data);
+        } e.stopPropagation();
 }
 
 /**
@@ -91,9 +83,7 @@ queryAPI()
  * Infinitive scroll
  */
 $(window).scroll(function (user) {
-    if ($(window).scrollTop() + $(window).height() == $(document).height()) {
-
-    }
-        queryAPI()
+    if ($(window).scrollTop() + $(window).height() == $(document).height()) { }
+    queryAPI()
 
 });     
