36710e3a62e19dac77fe57bc83b900047675f998
yeny <yeny.lopez@digitalamericas.ai>
2019-09-04 00:25:44 -0500

    Bug  fixed when the infinitive scroll is called

--- Challenge/script.js
+++ Challenge/script.js
@@ -6,44 +6,63 @@ let people = document.querySelector('#people')
 let person = document.querySelector('#person')
 let header = document.querySelector('#navOptions')
 let returnButton = document.querySelector('#returnButton')
-const API = 'https://randomuser.me/api/?results=100';
-let lengthUsers;
-let results
+const API = 'https://randomuser.me/api/?results=50';
 
+let results={};
+let countApiCall=0;
+let val=0;
 /**
  * Query
  */
-async function queryAPI () {
-    await fetch(API).then((value) =>{
+var promise1 = Promise.resolve();
+
+function queryAPI () {
+    fetch(API).then((value) =>{        
         return value.json();
-    }).then((value) => {         
-        lengthUsers = value.results.length;
-        createPhoto(value.results);
-        console.log(value)
-        results = value.results;      
+    }).then((value) => { 
+        countApiCall++;    
+        results = prepData(value.results,countApiCall); 
+        createPhoto(results,countApiCall); 
     }).catch(err => {
         console.log(err)
-    })
-    // .resolve('foo').then(() => {return Promise.resolve('bar')})
-    
+    })    
 }
 
 /**
  * Initialize the view 
  */
- 
 queryAPI()
 
 
 /**
+ * Prepare the data to have all the calls to the API in a single object
+ * 
  * @param {Input with the users array with all data} user
+ * @param {Number of calls performed to the API} numCall
  */
-function createPhoto(data) {   
+function prepData(data, numCall= 0) { 
+    let cont=0;
+    for (let index = numCall*50-50; index < Object.keys(data).length + numCall*50-50; index++) {
+        // results = {...results,...data[index]};  
+        results[index] = data[cont];
+        console.log(`Insertion:${index} == ${data[cont]}`);
+        cont++;
+    }
+    return results
+}
 
-    for (let index = 0; index < lengthUsers; index++) {
 
+/**
+ * Creates the new photos on the DOM when the API is called
+ * 
+ * @param {Input with the users array with all data} user
+ * @param {Number of calls performed to the API} numCall
+ */
+function createPhoto(data, numCall= 0) {   
+
+    for (let index = numCall*50-50; index < Object.keys(data).length + numCall*50-50; index++) {        
         localStorage.setItem('datos', JSON.stringify(data[index]));
-        
+        // console.log(data[index])
         // new DOM elements 
         let saved = localStorage.getItem('datos');
         let article = document.createElement('article')
@@ -65,12 +84,11 @@ function createPhoto(data) {
         clickImage.id = data[index].name.first
 
         // DOM structure
-        imagen.id = data[index].email;
-        // index;
+        imagen.id = index;
         article.appendChild(imagen)
         article.appendChild(nameImg)        
         photos.appendChild(article)
-        header.appendChild(clickImage)
+        header.appendChild(clickImage)             
     }
 
 }
@@ -87,9 +105,7 @@ returnButton.addEventListener('click',redirectHome, false )
  */
 function selectImage(event) {   
     //Certified that the click was performed on the image
-    if (event.target !== event.currentTarget) { 
-        console.log(event.target)
-
+    if (event.target !== event.currentTarget) {         
         people.style.display= "none";
         person.style.display= "block";
 
@@ -99,32 +115,7 @@ function selectImage(event) {
         let gender = document.querySelector('#gender')
         
         //Get the data of the target photo
-        let index = event.target.getAttribute('id');
-        console.log(typeof(results))
-        // console.log(results.keys)
-        let arrayResults = Object.keys(results)
-        console.log(arrayResults)
-        
-        var filtered = arrayResults.filter(person => {
-            console.log(arrayResults.email)
-            return arrayResults.email
-        })
-
-        // var filtered = [];
-        // arrayResults.each(arrayResults, function(){
-        //     if (this.email == index) {
-        //         filtered.push(this);
-        //     }
-        // });
-        console.log(filtered)
-
-        // console.log(arrayResults.filter(
-
-        //      this.email == index
-
-        //     ))
-        // console.log(index)
-        // console.log(results[index])
+        let index = event.target.getAttribute('id');        
         imgSelected.src = results[index].picture.large
 
         name.innerHTML = `Name: ${results[index].name.first}` 
@@ -143,13 +134,11 @@ function redirectHome(event){
 }
 
 
-
-
 /**
  * Infinitive scroll
  */
 window.onscroll = function() {    
-    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {        
+    if ((window.innerHeight + window.scrollY) >= document.body.offsetHeight) {            
         queryAPI()
     }
 };  
